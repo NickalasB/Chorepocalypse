3dd5acd77ba7d855899c5e4fee5c8475dc77ff36
Nick Bradshaw <nickalasb@gmail.com>
2017-03-05 06:23:03 -0800

    made removeAlarm() a local method inside adapter to successfully remove alarms when a chore is deleted

--- app/src/main/java/com/zonkey/chorepocalypse/ui/activities/AddChoreActivity.java
+++ app/src/main/java/com/zonkey/chorepocalypse/ui/activities/AddChoreActivity.java
@@ -66,7 +66,6 @@ public class AddChoreActivity extends AppCompatActivity implements TimePickerFra
     private boolean mDueDateSelected;
     private boolean mChoreNameSelected;
 
-
     @BindView(R.id.add_chore_name)
     EditText mChoreNameEditText;
 
@@ -328,14 +327,6 @@ public class AddChoreActivity extends AppCompatActivity implements TimePickerFra
         } else {
             alarmManager.set(AlarmManager.RTC_WAKEUP, mChoreTime, pendingIntent);
         }
-    }
-
-    public void removeAlarm(Chore chore) {
-        Toast.makeText(this, "Alarm Canceled", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this, AlarmReceiver.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, chore.getChoreKey().hashCode(), intent, 0);
-        AlarmManager alarmManager = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
-        alarmManager.cancel(pendingIntent);
     }
 
     // TODO: 2/27/17 figure out how to update widgets
